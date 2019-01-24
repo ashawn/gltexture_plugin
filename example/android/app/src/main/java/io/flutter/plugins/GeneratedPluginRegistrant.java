932c5cf14e0c53f2db20f27ead1f5ead4fbcfd6b
AShawn <ashawn@qq.com>
2019-01-24 16:09:01 +0800

    上传gltexture plugin

---
+++ example/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -0,0 +1,25 @@
+package io.flutter.plugins;
+
+import io.flutter.plugin.common.PluginRegistry;
+import com.gltexture_plugin.GLTexturePlugin;
+
+/**
+ * Generated file. Do not edit.
+ */
+public final class GeneratedPluginRegistrant {
+  public static void registerWith(PluginRegistry registry) {
+    if (alreadyRegisteredWith(registry)) {
+      return;
+    }
+    GLTexturePlugin.registerWith(registry.registrarFor("com.gltexture_plugin.GLTexturePlugin"));
+  }
+
+  private static boolean alreadyRegisteredWith(PluginRegistry registry) {
+    final String key = GeneratedPluginRegistrant.class.getCanonicalName();
+    if (registry.hasPlugin(key)) {
+      return true;
+    }
+    registry.registrarFor(key);
+    return false;
+  }
+}
---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <gltexture_plugin/GLTexturePlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [GLTexturePlugin registerWithRegistrar:[registry registrarForPlugin:@"GLTexturePlugin"]];
+}
+
+@end
